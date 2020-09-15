07338e0f182dc6ec27e88ba3eb6e70b72e99cc14
Shivam <vermashivam0606@gmail.com>
2020-09-15 11:54:10 +0530

    Updated views and frontend, added Procfile

--- main/views.py
+++ main/views.py
@@ -7,49 +7,46 @@ from main import models
 # Create your views here.
 
 BASE_CRAIGSLIST_URL = 'https://delhi.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_1200x900.jpg'
+
 
 def home(request):
     context = {}
     return render(request,'main/base.html',context)
 
 def new_search(request):
-    search  = request.POST.get('search')
+    search = request.POST.get('search')
     models.Search.objects.create(search=search)
-    final_url  = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
     response = requests.get(final_url)
     data = response.text
-    soup = BeautifulSoup(data,features="html.parser")
-    post_listings = soup.find_all('li',{'class':'result-row'})
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
     final_postings = []
-    print("hello")
+
     for post in post_listings:
         post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
-        try:
-            post_price = post.find('span',{'class':'result-price'}).text
-        except:
-            post_price = 'N/A'
-        #post_image = post.find(class_='result-image gallery')
-        #post_image = post_image.get('img').get('src')
-        new_response = requests.get(post_url)
-        new_data = new_response.text
-        soup = BeautifulSoup(new_data,features="html.parser")
-        post_img = soup.findAll('img')
-        try:
-            post_img = post_img[0].get('src')
-        except:
-            post_img = ''
-        # print(post_img)
 
-        # print(post_title)
-        # print(post_url)
-        # print(post_price)
-        final_postings.append((post_title,post_url,post_price,post_img))
-    
-    #print(final_postings)
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
     context = {
         'search': search,
-        'final_postings':final_postings
+        'final_postings': final_postings,
     }
+    
     return render(request,'main/new_search.html',context)
 
